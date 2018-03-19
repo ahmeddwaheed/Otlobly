1afb4795a03ab49efebe7a4e97f26f1ca462b459
sherif-ismail <sherif.ismail318@gmail.com>
2018-03-19 10:12:38 +0200

    Created User Model

--- Otlobly/db/schema.rb
+++ Otlobly/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318232113) do
+ActiveRecord::Schema.define(version: 20180319080942) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
